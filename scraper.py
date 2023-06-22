6f71c676ca6534bd4610b858aa4c2ebd3d75244c
Cambobug <86018292+Cambobug@users.noreply.github.com>
2023-06-22 16:43:13 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,10 +6,10 @@ import datetime as dt
 user_agent = "scraper_1.0"
 
 #Create an instance of Reddit class
-reddit = praw.Reddit(username="Cambobug",
-                     password="hufflepif89",
-                     client_id="RDpzdo5cuulGpE_518Ojhg",
-                     client_secret="37D_YU-2Ri_PRuyqSAhO57YpMyMCnQ",
+reddit = praw.Reddit(username="B L A N K", # reddit username would go here
+                     password="B L A N K", #reddit passwork would go here
+                     client_id="B L A N K", # client ID
+                     client_secret="B L A N K", # client secret
                      user_agent=user_agent)
 
 subreddit_name = "AskReddit"
@@ -29,15 +29,11 @@ for i in range(0, 5):
     post = reddit.submission(ids[i])
     post.comment_sort = "top" #sorts comments in post by "top" instead of default "hot"
     post.comments.replace_more(limit=0) #replaces "Load More Comments" buttons in the comment forest
-    #print("********" + titles[i] + "********\n")
     for j in range(0, 10): #loops through top 10 comments
         topComments[i].append(post.comments[j].body) #stores comment in topComments array
-        #print("Comment #" + str(j) + "\n")
-        #print(post.comments[j].body)
     topComments.append([])
     
 date = dt.datetime.now()
-#t = "gay"
 dateStr = date.strftime("%x")
 dateStr = dateStr.replace("/", "-")
 dateFile = open(str(dateStr) + ".txt", "w")
@@ -52,6 +48,3 @@ for i in range(0, 5):
             dateFile.write("Error: Comment contained an unreadable character!\n")
         
 dateFile.close()
-
-
-    
